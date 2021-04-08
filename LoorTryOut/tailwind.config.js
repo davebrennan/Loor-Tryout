b7a53cc5678dcdd740bf38bd0bcbc62bdc53390f
davebrennan <davethebabe713@gmail.com>
2021-04-07 22:17:36 -0700

    initial upload

---
+++ LoorTryOut/tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        loor: {
+          dark: {
+            100: '#222333',
+            300: '#42405C',
+            400: '#1E1D2B',
+            700: '#171621',
+            900: '#202020'
+          },
+          gold: '#FFBE03'
+        }
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
